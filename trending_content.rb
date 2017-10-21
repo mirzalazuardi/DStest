3f1cd095364a33e718f134f9b76d6f9f13c3b2cb
Mirzalazuardi Hermawan <mirzalazuardi@gmail.com>
2017-10-21 14:22:01 +0700

    add pre_suffix action & fixed count_level_subdir

--- trending_content.rb
+++ trending_content.rb
@@ -3,13 +3,24 @@ class TrendingContent
   def initialize(*args)
     namedir  = args[0] || '.'
     @namedir = namedir
+    @level_subdir = 0
+  end
+
+  def pre_suffix num
+    '/**' * num
   end
 
   def count_level_subdir
-    path       = @namedir+'/*'
-    pre_result = 0
-    subdir     = proc {Dir[path].select{ |x| Dir.exist? x }}
-    subdir.call().size
+    subdirs         = lambda { |str| Dir[@namedir + str + '/*'].select{ |node| Dir.exist? node }}
+    comparable_item = lambda { |num| subdirs.call(pre_suffix(num)).size }
+    curr_subdir_count = nil
+    loop do 
+      prev_subdir_count = (curr_subdir_count.nil? == true) ? comparable_item.call(@level_subdir) : curr_subdir_count
+      @level_subdir       += 1
+      curr_subdir_count = comparable_item.call(@level_subdir)
+      break if curr_subdir_count == prev_subdir_count
+    end
+    p @level_subdir
   end
 
   def exec
